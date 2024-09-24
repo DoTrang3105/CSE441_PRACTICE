e4594d8127d33ed53b057d3a924a2eaf60f2184c
Dotrang3105 <trangheo3105@gmail.com>
2024-09-24 16:31:10 +0700

    update lan2

--- Ex02/app/src/main/java/com/example/ex02/MainActivity.java
+++ Ex02/app/src/main/java/com/example/ex02/MainActivity.java
@@ -2,65 +2,58 @@ package com.example.ex02;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
- private Button btncall;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toast Toast = null;
-        Toast.makeText(this, "CR424 - onCreat()", Toast.LENGTH_SHORT).show();
-        btncall = findViewById(R.id.btncall);
-        btncall.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // TODO Auto-generated method stub
-                Intent intent1 = new Intent(MainActivity.this, Subactivity.class);
-                startActivity(intent1);
-            }
+        android.widget.Toast.makeText(this, "CR424 - onCreate()", android.widget.Toast.LENGTH_SHORT).show();
+        Button btnCall = findViewById(R.id.btn_call);
+        btnCall.setOnClickListener(v -> {
+            // TODO Auto-generated method stub
+            Intent intent1 = new Intent(MainActivity.this, SubActivity.class);
+            startActivity(intent1);
         });
     }
-
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
     }
+
     @Override
     protected void onPause() {
         // TODO Auto-generated method stub
         super.onPause();
         Toast.makeText(this," CR424 - onPause", Toast.LENGTH_SHORT).show();
     }
+
     @Override
     protected void onRestart() {
         // TODO Auto-generated method stub
         super.onRestart();
         Toast.makeText(this," CR424 - onRestart", Toast.LENGTH_SHORT).show();
     }
+
     @Override
     protected void onResume() {
         // TODO Auto-generated method stub
-
         super.onResume();
         Toast.makeText(this," CR424 - onResume", Toast.LENGTH_SHORT).show();
     }
+
     @Override
     protected void onStart() {
         // TODO Auto-generated method stub
         super.onStart();
         Toast.makeText(this," CR424 - onStart", Toast.LENGTH_SHORT).show();
     }
+
     @Override
     protected void onStop() {
         // TODO Auto-generated method stub
@@ -68,4 +61,4 @@ public class MainActivity extends AppCompatActivity {
         Toast.makeText(this," CR424 - onStop", Toast.LENGTH_SHORT).show();
     }
 
-    }
+}
