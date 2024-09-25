04cb71c93821d59d2a8b737403df0b5f6ba96cea
Dotrang3105 <trangheo3105@gmail.com>
2024-09-25 22:49:01 +0700

    saikitu

--- Ex02/app/src/main/java/com/example/ex02/MainActivity.java
+++ Ex02/app/src/main/java/com/example/ex02/MainActivity.java
@@ -20,7 +20,7 @@ public class MainActivity extends AppCompatActivity {
             startActivity(intent1);
         });
     }
-
+//Dotrang
     @Override
     protected void onDestroy() {
         super.onDestroy();
