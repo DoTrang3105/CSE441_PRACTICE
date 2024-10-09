d08cc38326391c5c0f0e38de74cb48bffcc1ea41
DoTrang3105 <trangheo3105@gmail.com>
2024-10-09 17:13:00 +0700

    updateEx07

--- Ex07/app/src/main/java/com/example/ex07/ChildActivity.java
+++ Ex07/app/src/main/java/com/example/ex07/ChildActivity.java
@@ -1,6 +1,23 @@
 package com.example.ex07;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ChildActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_child);
+        Button btn2 = (Button) findViewById(R.id.btnBack);
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent1 = new Intent(ChildActivity.this,MainActivity.class);
+                startActivity(intent1);
+            }
+        });
+    }
 }
