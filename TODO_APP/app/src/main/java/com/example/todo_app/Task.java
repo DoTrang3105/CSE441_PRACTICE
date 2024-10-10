78078b50a6dbc2aaa72271e63a8853359ac4eac0
DoTrang3105 <trangheo3105@gmail.com>
2024-10-11 00:16:19 +0700

    update

---
+++ TODO_APP/app/src/main/java/com/example/todo_app/Task.java
@@ -0,0 +1,16 @@
+package com.example.todo_app;
+
+public class Task {
+    public int id;
+    public String name;
+    public boolean completed;
+
+    public Task(int id, String name, boolean completed) {
+        this.id = id;
+        this.name = name;
+        this.completed = completed;
+    }
+}
+
+
+
