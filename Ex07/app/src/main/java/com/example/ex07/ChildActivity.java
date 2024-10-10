4ba3e2aaa77d2d6933daf3480135309153743081
DoTrang3105 <trangheo3105@gmail.com>
2024-10-10 15:26:37 +0700

    init

--- Ex07/app/src/main/java/com/example/ex07/ChildActivity.java
+++ Ex07/app/src/main/java/com/example/ex07/ChildActivity.java
@@ -5,13 +5,16 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class ChildActivity extends Activity {
+    private TextView txt2;
+    private Button btn2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_child);
-        Button btn2 = (Button) findViewById(R.id.btnBack);
+        Button btn2 = (Button) findViewById(R.id.btn2);
         btn2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
