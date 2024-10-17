ee248a1701ba1beab2725cc71c5c261595f86a71
DoTrang3105 <trangheo3105@gmail.com>
2024-10-17 16:12:04 +0700

    updatelayout

--- Ex14_01/app/src/main/java/com/example/ex14_01/MainActivity.java
+++ Ex14_01/app/src/main/java/com/example/ex14_01/MainActivity.java
@@ -6,6 +6,7 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TabHost;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,34 +16,4 @@ import androidx.core.view.WindowInsetsCompat;
 
 import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity {
-    EditText edta, edtb;
-    Button btnCong;
-    ListView lv1;
-    ArrayList<String> list;
-    ArrayAdapter<String> myarray;
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        addControl();
-        addEvent();
-    }
-
-    private void addEvent(){
-        btnCong.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                XuLyCong();
-            }
-            private void XuLyCong(){
-                int a = Integer.parseInt(edta.getText().toString());
-                int b = Integer.parseInt(edtb.getText().toString());
-                String c = a+" + "+b+" = "+(a+b);
-                list.add(c);
-                myarray.notifyDataSetChanged();
-                edta.setText("");
-                edtb.setText("");
-            }
-        });
-    }
+public
