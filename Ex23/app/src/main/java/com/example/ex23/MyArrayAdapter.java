c155f63afa327140e6c3cce9702a893a5c90e534
DoTrang3105 <trangheo3105@gmail.com>
2024-10-25 11:59:16 +0700

    main layout

---
+++ Ex23/app/src/main/java/com/example/ex23/MyArrayAdapter.java
@@ -0,0 +1,47 @@
+package com.example.ex23;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MyArrayAdapter extends ArrayAdapter {
+    private Activity context;
+    private ArrayList<List> arr;
+    private int layoutID;
+
+    public MyArrayAdapter( Activity context, ArrayList<List> arr, int layoutID) {
+        super(context, layoutID, arr);
+        this.context = context;
+        this.arr = arr;
+        this.layoutID = layoutID;
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        LayoutInflater inflater = context.getLayoutInflater();
+        convertView  = inflater.inflate(layoutID, null);
+        final List lst = arr.get(position);
+        ImageView imgitem = convertView.findViewById(R.id.imgView);
+        imgitem.setImageBitmap(lst.getImg());
+
+        TextView txtTitle = convertView.findViewById(R.id.txtTitle);
+        txtTitle.setText(lst.getTitle().toString());
+        TextView txtInfo = convertView.findViewById(R.id.txtInfo);
+        txtInfo.setText(lst.getInfo().toString());
+
+        return convertView;
+    }
+}
